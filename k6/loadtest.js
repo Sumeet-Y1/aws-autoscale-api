794b6e042b30f3d59e40cef8a377f68e0819c604
Sumeet-Y1 <sumeety202@gmail.com>
2026-06-08 13:56:08 +0530

    clean up k6 load test script

--- k6/loadtest.js
+++ k6/loadtest.js
@@ -8,7 +8,7 @@ export let options = {
     stages: [
         { duration: '30s', target: 100 },   // ramp up to 100 users
         { duration: '1m',  target: 500 },   // ramp up to 500 users
-        { duration: '1m',  target: 1000 },  // ramp up to 1000 users 🔥
+        { duration: '1m',  target: 1000 },  // ramp up to 1000 users 
         { duration: '30s', target: 0 },     // ramp down
     ],
     thresholds: {
